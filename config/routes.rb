e1ec3db137096c572484f5a8ede8c88cadb5e5b2
Thao <thaozers@gmail.com>
2017-04-22 16:19:58 -0700

    created article scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
 	resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\
   # can include/exclude HTTP actions using except, include etc, rename controller helper will remain the same i.e. resources :jobs[include or exclude]
